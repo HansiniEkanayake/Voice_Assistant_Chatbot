1c09dfbd04e71dba9eaee42ad9a8ec9b50ec8d8d
MahendraThammita <dhmmpthammita@gmail.com>
2021-01-01 00:31:24 +0530

    Data processing features added

--- Main.py
+++ Main.py
@@ -9,31 +9,85 @@ import json
 import random
 #ML libaries
 import nltk
+#nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
 
 import numpy
 import tensorflow
 import tflearn
 
+
+stemmer = LancasterStemmer()
+
 with open("Dset.json") as DsetFile:
     Dset = json.load(DsetFile)
 
-KeyWords = []
+keyWords = []
+tempKeyWords = []
 labels = []
 patternVals = []
 patternKeys = []
+identifiedSet = []
+output = []
 
 for DataBlock in Dset["intents"]:
     for Q_Pattern in DataBlock["patterns"]:
-        #Words in patterns are processed into its root words and store in KeyWords list.
-        KeyWords = nltk.word_tokenize(Q_Pattern)
+        #Words in patterns are seperated and store in KeyWords list.
+        miniPatternList = nltk.word_tokenize(Q_Pattern)
+        keyWords.extend(miniPatternList)
         #Patterns are add into the patternVals List.
-        patternVals.append(Q_Pattern)
+        patternVals.append(miniPatternList)
+        patternKeys.append(DataBlock["tag"])
         
         if DataBlock["tag"] not in labels:
             labels.append(DataBlock["tag"])
+print(keyWords)
+labels = sorted(labels)
 
+
+for word in keyWords:
+    #Stemming converts the words in keyWords list into their root words
+    tempKeyWords.append(stemmer.stem(word.lower()))
+
+#Sort the stemmed word list in alphabatic order and remove duplicates
+tempKeyWords = sorted(list(set(tempKeyWords)))
+keyWords = tempKeyWords
+
+print(keyWords)
+
+outList = [0 for _ in range(len(labels))]
+
+for x,vals in enumerate(patternVals):
+    binaryWordList = []
+
+    words = [stemmer.stem(root) for root in vals]
+
+    for word in keyWords:
+        if word in words:
+            binaryWordList.append(1)
+        else:
+            binaryWordList.append(0)
+            
+    binaryOut = outList[:]
+    binaryOut[labels.index(patternKeys[x])] = 1
+    
+    identifiedSet.append(binaryWordList)
+    output.append(binaryOut)
+    
+identifiedSet = numpy.array(identifiedSet)
+output = numpy.array(output)
+
+
+
+print(patternVals)
+print(labels)
+print(patternKeys)
+
+
+
+
+
+#Method for audio output using python text to speech
 def AudioOut(text):
     #Transfers the text to an audion file in English using gTTS
     #inText = gTTS(text=text , lang='en')
